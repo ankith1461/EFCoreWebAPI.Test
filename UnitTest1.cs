3278d2b514c056c960a99cb0fcea9470cda78b24
Ankith Reddy <ankith_reddy146@hotmail.com>
2017-10-13 14:03:18 +0800

    Unit Testing
    Unit Testing For Films Controller

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,10 +1,15 @@
 using System;
 using Xunit;
 using WebApiCoreEdx.Controllers;
+using WebApiCoreEdx.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace EFCoreWebAPI.Test
 {
+    /* Sample Test Class */
     public class UnitTest1
     {
         [Fact]
@@ -20,17 +25,47 @@ namespace EFCoreWebAPI.Test
         }
     }
 
-    public class ActorsControllerFunctionTest
-{
-    [Fact]
-    public void GetActorByIdSmokeTest()
+    /* Functional Test Case for Films Controller */
+    public class FilmsControllerFunctionTest
     {
-        var controller = new FilmsController();
-        var result = controller.GetFilm(101) as OkObjectResult;
-        var actor = result.Value as Actor;
-        Assert.Equal(actor.Actor_ID, 101);
-        Assert.Equal(actor.First_Name, "SUSAN");
-        Assert.Equal(actor.Last_Name, "DAVIS");
+        [Fact]
+        public void GetFilmByIdSmokeTest()
+        {
+            var controller = new FilmsController();
+            var result = controller.GetFilm(10);
+            
+            Assert.Equal(result.Film_ID, 10);
+            Assert.Equal(result.Title, "ALADDIN CALENDAR");
+            Assert.Equal(result.Release_Year, 2006);
+        }
     }
-}
+
+    /* End to End Test case for Films Controller */
+    public class FilmsControllerEndToEndTest
+    {
+        [Fact]
+        public async void GetFilmByIdSmokeTest()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:5000/");
+                var acceptType = new MediaTypeWithQualityHeaderValue("application/json");
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(acceptType);
+                var response = await httpClient.GetAsync("api/Films/10");
+                string jsonString = null;
+                if (response.IsSuccessStatusCode)
+                {
+                    jsonString = await response.Content.ReadAsStringAsync();
+                    Assert.NotNull(jsonString);
+                }
+
+                Film film = JsonConvert.DeserializeObject<Film>(jsonString);
+                Assert.NotNull(film);
+                Assert.Equal(film.Film_ID, 10);
+                Assert.Equal(film.Title, "ALADDIN CALENDAR");
+                Assert.Equal(film.Release_Year, 2006);
+            }
+        }
+    }
 }
